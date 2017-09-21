2fbd149db7ba26dbeadfd60cbe9b05d96935dd8b
Vishal Srinivasan <vissrinivasan@inautix.co.in>
2017-09-21 16:11:04 +0530

    configured mLab

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/apaarr'
+    'mongoUrl': 'mongodb://vishalsrini:Ramamani123@ds143774.mlab.com:43774/apaarr'
 }
