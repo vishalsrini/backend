6b67d49dbebbf0894ea30254e122bd89bb7a99aa
Apaarr Procurement Services <apaarr.ir@gmail.com>
2017-10-21 17:39:43 +0530

    changes in cors to make it more workable

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ mongoose.connect(config.mongoUrl, {
   useMongoClient: true
 });
 var db = mongoose.connection;
-db.on('error', function(err){
+db.on('error', function (err) {
   console.log(err);
 });
 
-db.once('open', function(){
+db.once('open', function () {
   console.log('connected successfully');
 });
 
@@ -34,10 +34,22 @@ db.once('open', function(){
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
-  next();
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+//   next();
+// });
+
+app.use(function (req, res, next) { 
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, x-access-token'); 
+  res.setHeader('Access-Control-Allow-Credentials', true); 
+  if ('OPTIONS' === req.method) { 
+    res.send(204); 
+  } else { 
+    next(); 
+  } 
 });
 
 // view engine setup
@@ -47,8 +59,8 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -75,14 +87,14 @@ app.use('/offers', offers);
 app.use('/negotiate', negotiations);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
