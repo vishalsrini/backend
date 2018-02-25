658eb5d66649c8ec8cd652d5a30c7300aacc528e
Vishal Srinivasan <vishalvishal619@gmail.com>
2018-02-25 17:42:10 +0530

    sending message while approve

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,15 @@ var verify = require('./verify');
 var cors = require('cors');
 var _ = require('lodash');
 
+var email = require("emailjs");
+
+var server = email.server.connect({
+    user: "vishalvishal619@gmail.com",
+    password: "Rama1768",
+    host: "smtp.gmail.com",
+    ssl: true
+});
+
 var adminRouter = express.Router();
 
 /** Using body parser for converting input to json */
@@ -55,6 +64,19 @@ adminRouter.route('/req/raw')
         _.forEach(req.body, (value) => {
             rawCashewReq.findByIdAndUpdate(value.id, { '$set': { status: value.status } }, { new: true }, function (err, resp) {
                 if (err) throw err;
+                server.send({
+                    text: "Hi " + value.name + ", \n Your Raw Cashew Requirement posted has been "+ value.status +".  \n  \n Message: " + value.message +" \n \n For More Details log on to https://app.apaarr.com \n \n Regards, \n Apaarr Procurement Services",
+                    from: "Vishal <vishalvishal619@gmail.com>",
+                    to: value.name + "<" + value.username + ">",
+                    bcc: "Vishal Srinivasan <vishalvishal619@gmail.com>",
+                    subject: "Reg your Requirement posted at APAARR PROCUREMENT SERVICES"
+                }, function (err, message) {
+                    console.log(err || message);
+                        if (err) {
+                            resp.status(200).json({ status: 'Something wrong', message: 'Something went wrong while sending mail' });
+                        };
+                    // return resp.status(200).json({ status: 'success', message: 'Successfully Negotiated' });
+                });
                 ++id;
                 if (id == req.body.length) {
                     res.status(200).json({
@@ -72,6 +94,19 @@ adminRouter.route('/req/processed')
         _.forEach(req.body, (value) => {
             processedCashewReq.findByIdAndUpdate(value.id, { '$set': { status: value.status } }, { new: true }, function (err, resp) {
                 if (err) throw err;
+                server.send({
+                    text: "Hi " + value.name + ", \n Your Processed Cashew Requirement posted has been "+ value.status+".  \n  \n Message: " + value.message +"\n \n For More Details log on to https://app.apaarr.com \n \n Regards, \n Apaarr Procurement Services",
+                    from: "Vishal <vishalvishal619@gmail.com>",
+                    to: value.name + "<" + value.username + ">",
+                    bcc: "Vishal Srinivasan <vishalvishal619@gmail.com>",
+                    subject: "Reg your Requirement posted at APAARR PROCUREMENT SERVICES"
+                }, function (err, message) {
+                    console.log(err || message);
+                        if (err) {
+                            resp.status(200).json({ status: 'Something wrong', message: 'Something went wrong while sending mail' });
+                        };
+                    // return resp.status(200).json({ status: 'success', message: 'Successfully Negotiated' });
+                });
                 ++id;
                 if (id == req.body.length) {
                     res.status(200).json({
@@ -89,6 +124,19 @@ adminRouter.route('/offers/raw')
         _.forEach(req.body, (value) => {
             rawCashew.findByIdAndUpdate(value.id, { '$set': { status: value.status } }, { new: true }, function (err, resp) {
                 if (err) throw err;
+                server.send({
+                    text: "Hi " + value.name + ", \n Your Raw Cashew Offer posted has been "+ value.status+".  \n  \n Message: " + value.message +" \n \n For More Details log on to https://app.apaarr.com \n \n Regards, \n Apaarr Procurement Services",
+                    from: "Vishal <vishalvishal619@gmail.com>",
+                    to: value.name + "<" + value.username + ">",
+                    bcc: "Vishal Srinivasan <vishalvishal619@gmail.com>",
+                    subject: "Reg your Offer posted at APAARR PROCUREMENT SERVICES"
+                }, function (err, message) {
+                    console.log(err || message);
+                        if (err) {
+                            resp.status(200).json({ status: 'Something wrong', message: 'Something went wrong while sending mail' });
+                        };
+                    // return resp.status(200).json({ status: 'success', message: 'Successfully Negotiated' });
+                });
                 ++id;
                 if (id == req.body.length) {
                     res.status(200).json({
@@ -106,6 +154,19 @@ adminRouter.route('/offers/processed')
         _.forEach(req.body, (value) => {
             processedCashew.findByIdAndUpdate(value.id, { '$set': { status: value.status } }, { new: true }, function (err, resp) {
                 if (err) throw err;
+                server.send({
+                    text: "Hi " + value.name + ", \n Your Processed Cashew Offer posted has been "+ value.status +".  \n  \n Message: " + value.message +"\n \n For More Details log on to https://app.apaarr.com \n \n Regards, \n Apaarr Procurement Services",
+                    from: "Vishal <vishalvishal619@gmail.com>",
+                    to: value.name + "<" + value.username + ">",
+                    bcc: "Vishal Srinivasan <vishalvishal619@gmail.com>",
+                    subject: "Reg your Offer posted at APAARR PROCUREMENT SERVICES"
+                }, function (err, message) {
+                    console.log(err || message);
+                        if (err) {
+                            resp.status(200).json({ status: 'Something wrong', message: 'Something went wrong while sending mail' });
+                        };
+                    // return resp.status(200).json({ status: 'success', message: 'Successfully Negotiated' });
+                });
                 ++id;
                 if (id == req.body.length) {
                     res.status(200).json({
